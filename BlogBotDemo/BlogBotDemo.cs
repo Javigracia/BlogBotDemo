58001a8a2a685db2d139500dc9d4fd56ffe22ae2
Javier Gracia Gonzalez <neodreamer@gmail.com>
2018-06-05 11:54:41 +0100

    fill project files

---
+++ BlogBotDemo/BlogBotDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Bot;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Threading.Tasks;
+
+namespace BlogBotDemo
+{
+    public class BlogBotDemo : IBot
+    {
+        public async Task OnTurn(ITurnContext turnContext)
+        {
+            if (turnContext.Activity.Type is ActivityTypes.Message)
+            {
+                string userInput = turnContext.Activity.Text;
+                await turnContext.SendActivity($"Has escrito {userInput}");
+            }
+        }
+    }
+}
